d7b2e8efea0137b2ce14decc18661bd7b776bedd
Ali <alihaidarlb3@gmail.com>
2025-11-25 20:18:52 +0200

    main physics engine done as well as base movement

--- platformer.py
+++ platformer.py
@@ -1,14 +1,35 @@
 import tkinter as tk
 
-WIDTH = 800
-HEIGHT = 800
+WIDTH = 1200
+HEIGHT = 1200
 class Game(object):
     def __init__(self):
         self.root = tk.Tk()
         self.canvas = tk.Canvas(self.root,width=WIDTH, height=HEIGHT, bg="black")
         self.canvas.pack()
-        self.root.mainloop()
+        self.person = Person(self.canvas)
+        self.person.create_person()
+        self.platforms = [Platform(self.canvas, 200, 700, 500, 60)]
+        self.root.bind("<KeyPress>", self.on_key_press)
+        self.root.bind("<KeyRelease>", self.on_key_release)
 
+        self.update_game()
+        self.root.mainloop()
+    def on_key_press(self, event):
+        if event.keysym == "Left":
+            self.person.vx = -5
+        elif event.keysym == "Right":
+            self.person.vx = 5
+        elif event.keysym == "space" and self.person.can_jump:
+            self.person.vy = -15
+            self.person.can_jump = False
+
+    def on_key_release(self, event):
+        if event.keysym in ("Left", "Right"):
+            self.person.vx = 0
+    def update_game(self):
+        self.person.update(self.platforms)
+        self.root.after(16, self.update_game)
 class Physics(object):
     GRAVITY = 1
     def __init__(self):
@@ -25,24 +46,33 @@ class Physics(object):
                 obj.y + obj.height > plat.y and obj.y < plat.y + plat.height):
                 obj.y = plat.y - obj.height
                 self.vy = 0
+                obj.can_jump = True
 
 class Person(Physics):
     def __init__(self, canvas):
+        super().__init__()
         self.canvas = canvas
-        self.player_width = 50
-        self.player_height = 50
-        self.player_x = WIDTH//2
-        self.player_y = HEIGHT // 2
+        self.width = 50
+        self.height = 50
+        self.x = WIDTH // 2
+        self.y = HEIGHT // 2
+        self.can_jump = False
+    def create_person(self):
+        self.rect = self.canvas.create_rectangle(self.x,self.y,self.x + self.width, self.y + self.height, fill ='red')
+    def update(self, platforms):
+        self.apply_gravity()
+        self.move(self)
+        self.check_collision(self, platforms)
+        self.canvas.coords(self.rect, self.x, self.y, self.x + self.width, self.y + self.height)
+
 
 class Platform(Physics):
-    WIDTH = 125
-    HEIGHT = 60
     def __init__(self, canvas, x, y,width,height):
         self.canvas = canvas
         self.x = x
         self.y = y
-        self.width = self.WIDTH
-        self.height = self.HEIGHT
+        self.width = width
+        self.height = height
         self.rect = canvas.create_rectangle(self.x,self.y, self.x + self.width, self.y + self.height ,fill = 'green')
         
 game = Game()
